e872996016f0d37c5124940a65973160ebe4c5ec
Gustav Moesmand <gustavmoesmand@gmail.com>
2021-06-15 21:20:14 +0200

    added new visualisations for thesis. Most work done in writing, and can be seen on overleaf

---
+++ Tools/__init__.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import matplotlib.colors as col
+
+DTU_COLS = {
+    "DTU_RED": (153, 0, 0),
+    "WHITE": (255, 255, 255),
+    "BLACK": (0, 0, 0),
+    "BLUE": (47, 62, 234),
+    "BRGREEN": (31, 208, 130),
+    "NABLUE": (3, 15, 79),
+    "YELLOW": (246, 208, 77),
+    "ORANGE": (252, 118, 52),
+    "PINK": (247, 187, 177),
+    "GREY": (218, 218, 218),
+    "RED": (232, 63, 72),
+    "GREEN": (0, 136, 53),
+    "PURPLE": (121, 35, 142)
+}
+
+class DTUColorMaps:
+    def custom_col_map(**cols):
+        pass
