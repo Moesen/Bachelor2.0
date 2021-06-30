24c0e5135a8b7e7651c0cf713ede0445e292f836
Gustav Moesmand <gustavmoesmand@gmail.com>
2021-06-30 15:31:52 +0200

    started framework for constructing graph

---
+++ SM/GraphConstructor.py
@@ -0,0 +1,6 @@
+# Gustav Lang Moesmand (s174169@student.dtu.dk)
+
+# Framework for constructing graphs from a given dataset.
+# Right now only works with mnist numbers
+# Pygel3d is used to visualise a lower dimensional representation of graph
+
